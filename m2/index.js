b6ac5721c3875af5a2228d212172e34201d651a7
bhagat-abhishek <inst.abhishek@gmail.com>
2024-05-09 01:37:04 +0530

    Handled Post Route, Added Nodemon for dev dependency

--- m2/index.js
+++ m2/index.js
@@ -3,5 +3,17 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json({ limit: '1mb' }));
+
+// Routes
+app.post("/api", (request, response) => {
+    
+    response.json({
+        status: "success",
+        latitute: request.body.lat,
+        longitude: request.body.long
+    })
+});
+  
 
 app.listen(3000, () => console.log("Server started on port: 3000"));
